d38ce1b77fdc455260f07a02d5ec89e2850772f7
Gulesh <gshukla@haverford.edu>
2023-12-11 20:22:00 -0500

    using atlas deployment

--- backend/app/src/main/java/com/config/MongoConfig.java
+++ backend/app/src/main/java/com/config/MongoConfig.java
@@ -1,27 +1,39 @@
 package com.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration{
 
-    
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
     @Override
     public MongoClient mongoClient()
     {
-        return MongoClients.create("mongodb://127.0.0.1:27017");
+         ConnectionString connectionString = new ConnectionString(mongoUri);
+         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
     }
 
     @Override
     protected String getDatabaseName()
     {
-        return "financeAppDB";
+        return databaseName;
     }
 
     @Override
